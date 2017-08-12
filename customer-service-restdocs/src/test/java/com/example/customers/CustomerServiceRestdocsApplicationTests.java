507eac67783c318cee48cf2d398ea1dbf6219cfd
Josh Long <josh@joshlong.com>
2017-08-11 22:55:56 -0400

    * generate stubs, Spring REST Docs, with the same MockMvc-based test.

--- customer-service-restdocs/src/test/java/com/example/customers/CustomerServiceRestdocsApplicationTests.java
+++ customer-service-restdocs/src/test/java/com/example/customers/CustomerServiceRestdocsApplicationTests.java
@@ -2,15 +2,63 @@ package com.example.customers;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.Arrays;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+
+
+@SpringBootTest(classes = CustomerServiceApplication.class)
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs(outputDir = "target/snippets")
 public class CustomerServiceRestdocsApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @MockBean
+    private CustomerRepository customerRepository;
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    private Customer c1 = new Customer(1L, "first", "last", "email@email.com");
+    private Customer c2 = new Customer(2L, "first", "last", "email@email.com");
+
+    @Test
+    public void getCustomerByIdShouldReturnCustomer() throws Exception {
+
+        Mockito
+                .when(this.customerRepository.findOne(1L))
+                .thenReturn(c1);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/customers/1"))
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(MockMvcResultMatchers.jsonPath("@.id").value(1L))
+                .andDo(MockMvcRestDocumentation.document("customerById"));
+    }
+
+    @Test
+    public void getCustomersShouldReturnAllCustomers() throws Exception {
+        Mockito
+                .when(this.customerRepository.findAll())
+                .thenReturn(Arrays.asList(c1, c2));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/customers"))
+                .andExpect(MockMvcResultMatchers.jsonPath("@.[0].id").value(1L))
+                .andExpect(MockMvcResultMatchers.jsonPath("@.[0].first").value("first"))
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andDo(document("customers"));
+    }
 }
