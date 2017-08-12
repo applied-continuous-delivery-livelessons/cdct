ed1bbcea3126249b28dfcb7f311f03cba933d918
Josh Long <josh@joshlong.com>
2017-08-12 13:41:39 -0400

    reworked the client

--- customer-client/src/test/java/com/example/customers/CustomerClientRestDocsStubTest.java
+++ customer-client/src/test/java/com/example/customers/CustomerClientRestDocsStubTest.java
@@ -7,9 +7,8 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.properties.PropertyMapping;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Collection;
@@ -21,7 +20,8 @@ import static org.hamcrest.Matchers.contains;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = CustomerClientApplication.class, properties = {"customer-service.host=http://localhost:8002"})
-@AutoConfigureWireMock(stubs = "classpath:/META-INF/com.example/customer-service-restdocs/**/*.json", port = 8002)
+//@AutoConfigureWireMock(stubs = "classpath:/META-INF/com.example/customer-service-restdocs/**/*.json", port = 8002)
+@AutoConfigureStubRunner(ids = "com.example:customer-service-restdocs:+:stubs:8002", workOffline = true)
 public class CustomerClientRestDocsStubTest {
 
     @Autowired
---
+++ customer-service-contracts-messaging/src/test/java/com/example/customers/CustomerBase.java
@@ -0,0 +1,42 @@
+package com.example.customers;
+
+import com.jayway.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+
+/**
+ * @author <a href="josh@joshlong.com">Josh Long</a>
+ */
+@SpringBootTest(classes = CustomerServiceApplication.class)
+@RunWith(SpringRunner.class)
+public class CustomerBase {
+
+    @MockBean
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private CustomerRestController customerRestController;
+
+    @Before
+    public void before() {
+
+        Mockito.when(this.customerRepository.findAll())
+                .thenReturn(Arrays.asList(
+                        new Customer(1L, "first1", "last1", "email1@email.com"),
+                        new Customer(2L, "first2", "last2", "email2@email.com")));
+
+        Mockito.when(this.customerRepository.findOne(Mockito.anyLong())).thenAnswer(invocation -> {
+            Object id = invocation.getArguments()[0];
+            return new Customer(Long.class.cast(id), "first", "last", "email@email.com");
+        });
+
+        RestAssuredMockMvc.standaloneSetup(this.customerRestController);
+    }
+}
