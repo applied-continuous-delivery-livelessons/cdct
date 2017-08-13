e83c25ee50c0be940fffa8755c27850db7ea9e0b
Josh Long <josh@joshlong.com>
2017-08-12 20:36:24 -0400

    yaml!!

--- customer-client/src/test/java/com/example/client/NotificationProcessorTest.java
+++ customer-client/src/test/java/com/example/client/NotificationProcessorTest.java
@@ -1,7 +1,6 @@
 package com.example.client;
 
 import org.assertj.core.api.BDDAssertions;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,9 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.stubrunner.StubTrigger;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 import org.springframework.cloud.stream.messaging.Sink;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageHandler;
-import org.springframework.messaging.MessagingException;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
@@ -27,9 +23,6 @@ public class NotificationProcessorTest {
     private NotificationProcessor processor;
 
     @Autowired
-    private Sink sink;
-
-    @Autowired
     private StubTrigger stubTrigger;
 
     @Before
@@ -39,15 +32,6 @@ public class NotificationProcessorTest {
 
     @Test
     public void notificationProcessorShouldReceiveANewCustomerReport() throws Throwable {
-        Assert.assertNotNull(this.sink);
-
-        this.sink.input().subscribe(new MessageHandler() {
-            @Override
-            public void handleMessage(Message<?> message) throws MessagingException {
-                System.out.println("Hi" + message.getPayload() + "");
-            }
-        });
-//        this.sink.input().send(MessageBuilder.withPayload("Hi").build());
         this.stubTrigger.trigger("customer_report_message");
         BDDAssertions.then(this.processor.reports.size()).isEqualTo(1);
     }
